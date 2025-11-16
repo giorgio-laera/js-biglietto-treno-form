b0e786c60ec8ec24bdf4284d89311739ba642685
giorgio laera <laeragiorg@gmail.com>
2025-11-16 17:50:39 +0100

    complete

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const distanceEl = document.getElementById("distance");
 const ageEl = document.getElementById("age");
 const sendEl = document.getElementById("send");
 const resetEl = document.getElementById("reset");
+const nameEl = document.getElementById("inputName");
+
 sendEl.addEventListener("click", function (event) {
     event.preventDefault();
 
-    console.log(distanceEl, ageEl.value);
+    console.log(distanceEl.value, ageEl.value);
 
     if (distanceEl == NaN) {
         console.log("non e un numero")
@@ -16,15 +18,22 @@ sendEl.addEventListener("click", function (event) {
     console.log(priceKm);
 
     let discount = 1;
-
+    let supplyEl=""
     if (ageEl.value == "minorenne") {
         discount = 0.8;
+        supplyEl = "Biglietto junior";
     } else if (ageEl.value == "senior") {
         discount = 0.6;
-    } else { }
+        supplyEl = "Biglietto senior";
+    } else {
+        supplyEl = "Biglietto standard";
+    }
 
     let result = priceKm * discount;
     document.getElementById("cost").innerHTML = result;
+    document.getElementById("name").innerText = nameEl.value;
+    document.getElementById("supply").innerText = supplyEl;
+    document.getElementById("footer").classList.remove("d-none")
 })
 reset.addEventListener("click", function () {
     distanceEl = "";
